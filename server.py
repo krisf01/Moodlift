2da9bcfb0758b09ae97520bd25d4d234383f6fad
Kristina <kfout@ucsc.edu>
2024-04-19 10:55:27 -0700

    react, css, html, server.py

--- server.py
+++ server.py
@@ -1,15 +1,15 @@
-#test file
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # This enables CORS for all domains on all routes
 
-@app.route('/')
+@app.route('/public/')
 def home():
     return "Hello, React frontend! This is Flask speaking."
 
 @app.route('/api/data')
 def get_data():
-    # Example data
     data = {
         "message": "Here's some data from the Flask server",
         "items": [1, 2, 3, 4, 5]
