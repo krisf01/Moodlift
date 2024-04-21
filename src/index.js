bfb4eeb4e67070086a7d6b3f4ab0361055229e86
Kristina <kfout@ucsc.edu>
2024-04-21 14:17:07 -0700

    navigation bar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import DataFetcher from './DataFetcher'; // Ensure this path is correct
-import './app.css'; // This is fine as long as app.css is in the same directory as index.js
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { NavigationBar, DataFetcher, JournalingPage } from './DataFetcher'; // Use named imports here
+import './app.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <DataFetcher />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+      <Router>
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<DataFetcher />} />
+          <Route path="/journaling" element={<JournalingPage />} />
+        </Routes>
+      </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
