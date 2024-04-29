134696f964c01c18a2538737fe4ab35ed2686d20
Kristina <kfout@ucsc.edu>
2024-04-28 19:32:29 -0700

    beautified the home page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { NavigationBar, DataFetcher, JournalingPage, MoodTrackPage, ResourcePage } from './DataFetcher'; // Use named imports here
+import { DataFetcher, JournalingPage, MoodTrackPage, ResourcePage } from './DataFetcher'; // Use named imports here
 import './app.css';
 
 ReactDOM.render(
     <React.StrictMode>
       <Router>
-        <NavigationBar />
         <Routes>
           <Route path="/" element={<DataFetcher />} />
           <Route path="/journaling" element={<JournalingPage />} />
