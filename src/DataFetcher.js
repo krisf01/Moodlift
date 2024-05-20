b56d1eef7007e57aa300283a997597757fc627bb
Emely Pineda <eppineda@ucsc.edu>
2024-05-19 17:35:52 -0700

    fix updated journaling page

--- src/DataFetcher.js
+++ src/DataFetcher.js
@@ -70,12 +70,8 @@ function Sidebar(){
 
 export const SidebarData = [
     {
-        title: "view public posts",
+        title: "view private posts",
         link: "/friendspost",
-    },
-    {
-        title: "view private entries",
-        link: "/savedpost",
     },
     {
         title: "new entry",
@@ -170,12 +166,11 @@ export function JournalingPage() {
                     cols={70}
                 ></textarea>
             </div>
-
         <div>
-            <Link to="/savedpost" className='savedbutton'>Saved</Link>
-        </div>
-        <div>
-            <button className='postbutton' onClick={handlePostToServer}>Post</button>
+            <Link to='/friendspost'> 
+                <button className='postbutton' onClick={handlePostToServer}>Post</button>
+            </Link>
+            
     </div>
     <div>
         <Sidebar />
@@ -185,87 +180,6 @@ export function JournalingPage() {
 }
 
 
-
-export function SavedPostPage(){
-    const[moodliftjournalprompt,setmoodliftJournalPrompt] = useState("Journal Prompt");
-
-    //handler for journal propmt changes
-    const handleMoodLiftJournalPromptChange =(e) => {
-        setmoodliftJournalPrompt(e.target.value);
-    };
-    //handler for saved input changes
-    const[moodliftjournalinput,setsavedjournalprompt] = useState(" this is my journal entry deafult text");
-
-    const handleSavedPostPromptChange =(e) => {
-        setsavedjournalprompt(e.target.value);
-    };
-     // Function to get the current date in the format "Month Day, Year"
-     const getCurrentDateSavedPost = () => {
-        const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        return new Date().toLocaleDateString('en-US', options);
-    };
-    //second saved input changes
-    const[moodliftjournalinput1,setsavedjournalprompt1] = useState(" this is my journal entry default text");
-
-    const handleSavedPostPromptChange1 =(e) => {
-        setsavedjournalprompt1(e.target.value);
-    };
-    
-    //second handler
-    const[moodliftjournalprompt1,setmoodliftJournalPrompt1] = useState("Journal Prompt");
-
-    const handleMoodLiftJournalPromptChange1 =(e) => {
-        setmoodliftJournalPrompt1(e.target.value);
-    };
-    return(
-        <div className='app-container'>
-            <div className='purple-rectangle'>
-                <Link to="/" className="home-link"> {/* Link to the home page */}
-                    <img src={homeIcon} alt="Home" className="home-icon" /> {/* Home icon */}
-                </Link>
-            </div>
-            <div>
-                <button className='borderbutton1'> </button>
-            </div>
-            <div>
-                <button className='borderbutton2'> </button>
-            </div>
-            <div>
-                <input className='moodliftprompt'
-                    value={moodliftjournalprompt}
-                    onChange={handleMoodLiftJournalPromptChange}
-                    readOnly={true}
-                ></input>
-            </div>
-    
-           <div>
-                <textarea className='journalinput1'
-                    value={moodliftjournalinput}
-                    onChange={handleSavedPostPromptChange}
-                    readOnly={true}
-                    rows = {15}
-                    cols = {62}
-                ></textarea>
-                <p className="date-text1">{getCurrentDateSavedPost()}</p> {/* Date text */}
-            <div>
-            <input className='moodliftprompt1'
-                    value={moodliftjournalprompt1}
-                    onChange={handleMoodLiftJournalPromptChange1}
-                    readOnly={true}
-                ></input>
-            </div>
-            <div> <Sidebar /> </div>
-           </div>
-                <textarea className="journalinput2"
-                    value={moodliftjournalinput1}
-                    onChange={handleSavedPostPromptChange1}
-                    readOnly={true}
-                    rows = {15}
-                    cols = {62}
-                ></textarea>
-        </div>
-    );
-}
 
 //updated friends page 
 export function FriendsPostPage() {
@@ -337,46 +251,38 @@ export function FriendsPostPage() {
     return (
         <div className='app-container friends-post-page-container'>
             <div className='purple-rectangle'>
-                <div style={{ position: 'absolute', top: '30px', left: '20px' }}>
-                    <h1 style={{ color: 'black' }}>Friends</h1>
-                </div>
                 <Link to="/" className="home-link">
                     <img src={homeIcon} alt="Home" className="home-icon" />
                 </Link>
+                <p className="date-text">{getCurrentDate()}</p>
+                <div className="moodlift-text">MoodLift</div>
             </div>
-            <div>
-                <button onClick={handlePost}>Post Data</button>
-            </div>
-            <div>
+            <div className='zindex'>
                 <input className='journalPrompt friends-journal-prompt-input'
                     value={journalPrompt}
                     onChange={handleJournalPromptChange}
                     readOnly={true}
                 />
             </div>
-            <div>
-                <textarea className='journalEntry friends-journal-input'
+            <div className='zindex'>
+                <textarea className=' friends-journal-input'
                     value={journalEntry}
                     onChange={handleJournalEntryChange}
                     readOnly={true}
                     rows={15}
                     cols={62}
                 />
-                <p className="date-text">{getCurrentDate()}</p>
-                <input className='nameInput friends-name-input'
-                    placeholder='Name:'
-                    value={name}
-                    onChange={handleNameChange}
-                />
             </div>
-            <div>
+            <div><button className='borderbutton4'></button>
+                </div>
+            <div className='zindex'>
                 <input className='journalPrompt1 friends-journal-prompt-input'
                     value={journalPrompt1}
                     onChange={handleJournalPromptChange1}
                     readOnly={true}
                 />
             </div>
-            <div>
+            <div className='zindex'>
                 <textarea className="journalEntry1 friends-journal-input"
                     value={journalEntry1}
                     onChange={handleJournalEntryChange1}
@@ -384,11 +290,9 @@ export function FriendsPostPage() {
                     rows={15}
                     cols={62}
                 />
-                <input className='nameInput1 friends-name-input'
-                    placeholder='Name:'
-                    value={name1}
-                    onChange={handleNameChange1}
-                />
+            </div>
+            <div>
+                <button className='borderbutton5'></button>
             </div>
             <div> {/* Placeholder for Sidebar component */}
                 <Sidebar />
