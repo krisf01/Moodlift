b4315741fa047846253efebb518b7873757ba48d
Emely Pineda <eppineda@ucsc.edu>
2024-05-28 18:03:15 -0700

    styled friends entries

--- src/FriendsJournalEntries.js
+++ src/FriendsJournalEntries.js
@@ -1,4 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import './app.css';
+import logoSVG from './images/logo.svg';
+import homeIcon from './images/home.svg';
+import { useNavigate } from 'react-router-dom';
 
 function FriendsJournalEntries() {
     const [journalEntries, setJournalEntries] = useState([]);
@@ -22,14 +27,48 @@ function FriendsJournalEntries() {
             .catch(error => console.error('Error fetching friends\' journal entries:', error));
     }, [userId]);
 
+    const getCurrentDate = () => {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        return new Date().toLocaleDateString('en-US', options);
+    };
+
     return (
-        <div className="friends-journal-entries">
-            <h3>Friends' Journal Entries</h3>
+        <div className="entries-container">
+            <div className='purple-rectangle'>
+                <Link to="/home" className="home-link">
+                    <img src={homeIcon} alt="Home" className="home-icon" />
+                </Link>
+                <p className="date-text">{getCurrentDate()}</p>
+                <div className="moodlift-text">MoodLift</div>
+            </div>
+            
+            <h3 className='friends-name'>Friends' Journal Entries</h3>
             {journalEntries.map(entry => (
-                <div key={entry.timestamp} className="journal-entry">
+                <div key={entry.timestamp} >
+
                     <p><strong>{entry.friend_email}</strong></p>
-                    <p>{entry.prompt}</p>
-                    <p>{entry.entry}</p>
+                   <div className='prompt-container'>
+                    <button className='borderbutton5'>
+                    <input
+                        className='moodliftprompt4'
+                        value ={entry.prompt}
+                        readOnly
+                    />
+                    
+                    <div>
+                
+                    <textarea
+                        className='journalinput5'
+                        value={entry.entry}
+                        readOnly
+                        rows={10}
+                        cols={50}
+                        />
+                    
+                   </div>
+                   </button>
+                   </div>
+                   
                     <p>{new Date(entry.timestamp).toLocaleString()}</p>
                 </div>
             ))}
